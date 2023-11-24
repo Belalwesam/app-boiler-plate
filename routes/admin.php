1f64f3af7b94fbefb4a61fe646e57bbbb83a0412
unknown <belalwesam3@gmail.com>
2023-11-25 01:26:31 +0300

    simple naming in admin.php routes file

--- routes/admin.php
+++ routes/admin.php
@@ -5,12 +5,12 @@ use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Admin Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Here is where you can register admin routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| be assigned to the "admin" middleware group. Make something great!
 |
 */
 
