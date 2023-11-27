5aeb20af10042b0a27a6d117f40609ab35bc28fb
unknown <belalwesam3@gmail.com>
2023-11-27 23:31:08 +0300

    added the footer file

--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -67,49 +67,14 @@
                     <!-- Content -->
 
                     <div class="container-fluid flex-grow-1 container-p-y">
-                        <!-- Layout Demo -->
-                        <div class="layout-demo-wrapper">
-                            <div class="layout-demo-placeholder">
-                                <img src="../../assets/img/layouts/layout-fluid-light.png" class="img-fluid"
-                                    alt="Layout fluid" data-app-light-img="layouts/layout-fluid-light.png"
-                                    data-app-dark-img="layouts/layout-fluid-dark.png" />
-                            </div>
-                            <div class="layout-demo-info">
-                                <h4>Layout fluid</h4>
-                                <p>Fluid layout sets a <code>100% width</code> at each responsive breakpoint.</p>
-                            </div>
-                        </div>
-                        <!--/ Layout Demo -->
+                        <!-- Pages Content Here -->
+                            Here
+                        <!--/ Pages Content Here -->
                     </div>
                     <!-- / Content -->
 
                     <!-- Footer -->
-                    <footer class="content-footer footer bg-footer-theme">
-                        <div
-                            class="container-fluid d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
-                            <div class="mb-2 mb-md-0">
-                                ©
-                                <script>
-                                    document.write(new Date().getFullYear());
-                                </script>
-                                , made with ❤️ by
-                                <a href="https://pixinvent.com" target="_blank"
-                                    class="footer-link fw-semibold">PIXINVENT</a>
-                            </div>
-                            <div>
-                                <a href="https://themeforest.net/licenses/standard" class="footer-link me-4"
-                                    target="_blank">License</a>
-                                <a href="https://1.envato.market/pixinvent_portfolio" target="_blank"
-                                    class="footer-link me-4">More Themes</a>
-
-                                <a href="https://pixinvent.com/demo/frest-clean-bootstrap-admin-dashboard-template/documentation-bs5/"
-                                    target="_blank" class="footer-link me-4">Documentation</a>
-
-                                <a href="https://pixinvent.ticksy.com/" target="_blank"
-                                    class="footer-link d-none d-sm-inline-block">Support</a>
-                            </div>
-                        </div>
-                    </footer>
+                    @include('admin.partials.footer')
                     <!-- / Footer -->
 
                     <div class="content-backdrop fade"></div>
