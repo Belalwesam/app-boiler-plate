21ad2f78f721db7cf163b016154f499eaf4d5f92
unknown <belalwesam3@gmail.com>
2023-11-29 00:17:34 +0300

    continue working on the project from auth points

--- app/Http/Controllers/Admin/Auth/AuthController.php
+++ app/Http/Controllers/Admin/Auth/AuthController.php
@@ -9,6 +9,10 @@ class AuthController extends Controller
 {
     public function login(Request $request)
     {
+        #todos 
+        #implement login using the username or email
+        #use the custom guard called admins to authenticate routes 
+        #work on roles , permissions and seeders for super admin
         dd($request->all());
     }
 }
