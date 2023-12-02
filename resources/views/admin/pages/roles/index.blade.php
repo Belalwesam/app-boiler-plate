d7dc3fc44e946102969abcb0577386fed2882960
unknown <belalwesam3@gmail.com>
2023-12-02 17:42:20 +0300

    Added the edit modal and form

--- resources/views/admin/pages/roles/index.blade.php
+++ resources/views/admin/pages/roles/index.blade.php
@@ -27,7 +27,7 @@
                         <div class="d-flex justify-content-between align-items-end">
                             <div class="role-heading">
                                 <h4 class="mb-1">{{ $role->name }}</h4>
-                                <a href="javascript:;" data-bs-toggle="modal" data-bs-target="#addRoleModal"
+                                <a href="javascript:;" data-bs-toggle="modal" data-bs-target="#editRoleModal"
                                     data-name = "{{ $role->name }}"
                                     data-permissions = "{{ $role->permissions->pluck('id') }}"
                                     class="role-edit-btn"><small>@lang('roles.edit_role')</small></a>
@@ -153,8 +153,91 @@
         </div>
     </div>
     <!--/ Add Role Modal -->
-
     <!-- / Add Role Modal -->
+
+
+
+
+    <!-- Edit Role Modal -->
+    <!-- Edit Role Modal -->
+    <div class="modal fade" id="editRoleModal" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered modal-edit-new-role">
+            <div class="modal-content p-3 p-md-5">
+                <button type="button" class="btn-close btn-pinned" data-bs-dismiss="modal" aria-label="Close"></button>
+                <div class="modal-body">
+                    <div class="text-center mb-4">
+                        <h3 class="role-title">@lang('roles.edit_role')</h3>
+                    </div>
+                    <!-- Edit role form -->
+                    <form id="editRoleForm" class="row g-3 fv-plugins-bootstrap5 fv-plugins-framework"
+                        onsubmit="return false" novalidate="novalidate">
+                        <div class="col-12 mb-4 fv-plugins-icon-container">
+                            <label class="form-label" for="edit_name">@lang('roles.role_name')</label>
+                            <input type="text" id="edit_name" name="edit_name" class="form-control"
+                                placeholder="@lang('roles.role_name')" tabindex="-1">
+                            <div class="fv-plugins-message-container invalid-feedback"></div>
+                        </div>
+                        <div class="col-12">
+                            <h5>@lang('roles.permissions')</h5>
+                            <!-- Permission table -->
+                            <div class="table-responsive">
+                                <table class="table table-flush-spacing">
+                                    <tbody>
+                                        <tr>
+                                            <td class="text-nowrap">
+                                                @lang('roles.adminstrator_access')
+                                                <i class="bx bx-info-circle bx-xs" data-bs-toggle="tooltip"
+                                                    data-bs-placement="top" title=""
+                                                    data-bs-original-title="Allows a full access to the system"
+                                                    aria-label="Allows a full access to the system"></i>
+                                            </td>
+                                            <td>
+                                                <div class="form-check">
+                                                    <input class="form-check-input" type="checkbox"
+                                                        id="selectAllCheckbox">
+                                                    <label class="form-check-label" for="selectAll">
+                                                        @lang('general.select_all')</label>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        @foreach ($permissions as $key => $value)
+                                            <tr>
+                                                <td class="text-nowrap">{{ ucwords($key) }}</td>
+                                                <td>
+                                                    <div class="d-flex">
+                                                        @foreach ($value as $permission)
+                                                            <div class="form-check me-3 me-lg-5">
+                                                                <input class="form-check-input edit_permission-checkbox"
+                                                                    value={{ $permission->id }} type="checkbox">
+                                                                <label class="form-check-label"> {{ $permission->name }}
+                                                                </label>
+                                                            </div>
+                                                        @endforeach
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- Permission table -->
+                        </div>
+                        <div class="col-12 text-center">
+                            <button type="button" id="submit-edit-btn"
+                                class="btn btn-primary me-sm-3 me-1">@lang('general.edit')</button>
+                            <button type="reset" class="btn btn-label-secondary" data-bs-dismiss="modal"
+                                aria-label="Close">
+                                @lang('general.cancel')
+                            </button>
+                        </div>
+                    </form>
+                    <!--/ Edit role form -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/ Edit Role Modal -->
+    <!-- / Edit Role Modal -->
 @endsection
 
 @section('script')
@@ -213,8 +296,11 @@
 
 
             //clicking the edit button transfers data to the form 
-            $('body').on('click' , '.role-edit-btn' , function() {
-                console.log('yes')
+            $('body').on('click', '.role-edit-btn', function() {
+                let permissions = $(this).data('permissions')
+                let name = $(this).data('name')
+
+                $('#edit_name').val(name)
             })
         })
     </script>
