e873f720b6ecbc10d21e60f68d214b829ca01768
unknown <belalwesam3@gmail.com>
2023-12-03 21:36:26 +0300

    formatting for the file

--- resources/views/admin/pages/admins/index.blade.php
+++ resources/views/admin/pages/admins/index.blade.php
@@ -1,27 +1,29 @@
 @extends('admin.layout.app')
-@section('css-vendor')
+
 @section('title')
     @lang('nav.admins')
 @endsection
-<link rel="stylesheet" href="{{ asset('/dashboard/assets/vendor/libs/datatables-bs5/datatables.bootstrap5.css') }}" />
-<link rel="stylesheet"
-    href="{{ asset('/dashboard/assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css') }}" />
+
+@section('css-vendor')
+    <link rel="stylesheet" href="{{ asset('/dashboard/assets/vendor/libs/datatables-bs5/datatables.bootstrap5.css') }}" />
+    <link rel="stylesheet"
+        href="{{ asset('/dashboard/assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css') }}" />
 @endsection
+
+{{-- main content --}}
 @section('content')
-<h4 class="py-3 breadcrumb-wrapper mb-2">@lang('admins.admins')</h4>
-<!--/ Role cards -->
+    <h4 class="py-3 breadcrumb-wrapper mb-2">@lang('admins.admins')</h4>
 @endsection
+
+
 @section('script-vendor')
-<script src="{{ asset('/dashboard/assets/vendor/libs/datatables/jquery.dataTables.js') }}"></script>
-<script src="{{ asset('/dashboard/assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js') }}"></script>
-<script src="{{ asset('/dashboard/assets/vendor/libs/datatables-responsive/datatables.responsive.js') }}"></script>
-<script src="{{ asset('/dashboard/assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.js') }}"></script>
+    <script src="{{ asset('/dashboard/assets/vendor/libs/datatables/jquery.dataTables.js') }}"></script>
+    <script src="{{ asset('/dashboard/assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js') }}"></script>
+    <script src="{{ asset('/dashboard/assets/vendor/libs/datatables-responsive/datatables.responsive.js') }}"></script>
+    <script src="{{ asset('/dashboard/assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.js') }}"></script>
 @endsection
 @section('script')
-<script>
-    $('document').ready(function() {
-
-
-    })
-</script>
+    <script>
+        $('document').ready(function() {})
+    </script>
 @endsection
